5a4bdcdd3364e2491153502313b5b5095a6835ea
imworseinperson <sebasti.sqg.2005@gmail.com>
2023-09-06 17:36:49 -0500

    parcial

--- Parcial1/parcial.py
+++ Parcial1/parcial.py
@@ -1,7 +1,12 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def Function(x):
     return np.exp(-x)-x
 
-x = np.linspace(-1,1, 100)
+x = np.linspace(-1,1, 200)
 y=Function(x)
+plt.plot(x,y)
+plt.axhline(y=0, color='r')
+plt.show()
+
