58de0e2ff798dea020ce3902bd2525a7b78f0468
imworseinperson <sebasti.sqg.2005@gmail.com>
2023-09-06 18:18:36 -0500

    parcial

--- Parcial1/parcial.py
+++ Parcial1/parcial.py
@@ -10,3 +10,40 @@ plt.plot(x,y)
 plt.axhline(y=0, color='r')
 plt.show()
 
+def muller(x):
+    x0 = x[125]
+    x1 = x[175]
+    x2 = x[150]
+    x3 = 99999999 
+
+    i=0
+
+    while (i < 100) and abs((Function(x3)) < 1*(10**-10)):
+
+        difx0x1 = (Function(x1)-Function(x0))/(x1-x0)
+        difx1x2 = (Function(x2)-Function(x1))/(x2-x1)
+        segunda = (difx1x2-difx0x1)/(x2-x0)
+        a = segunda
+        b = difx0x1-(x0+x1)*a
+        c = Function(x0)-(x0*difx0x1)+(x0*x1*a)
+
+
+        if b < 0:
+            x3=(-2*c)/(b-(np.sqrt((b**2)-(4*a*c))))
+        else:
+            x3=(-2*c)/(b-(np.sqrt((b**2)+(4*a*c))))
+        if x3 > 0:
+            x0 = x3
+        else:
+            x1 = x3
+        if x2 > x0 or x2 < x1:
+            x2 = (x0+x1)/2
+        
+        i += 1
+
+        print(x3)
+        print(i)
+    
+    return x3
+
+print(muller(x))
