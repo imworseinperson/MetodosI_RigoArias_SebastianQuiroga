9cf4fb6f13e21f2709796384c204bf08c574eb1e
RigoArias <r.ariasc@uniandes.edu.co>
2023-11-03 23:31:55 -0500

    TERMINE

--- Taller 3/Punto 2.py
+++ Taller 3/Punto 2.py
@@ -1,111 +1,54 @@
 import numpy as np
 
-def gauss(A, b):
+def eliminacion_gaussiana(A,B): 
      
-     M = np.hstack((A, b))
-     n, m = M.shape
-     x = np.zeros(n)
-     print(M)
-     # Recorrer cada fila de la matriz
-     for i in range(n):
-     # Buscar el pivote más grande en la columna i
-          max_row = i
-          max_val = abs(M[i, i])
-          print(M)
-          for j in range(i + 1, n):
-               if abs(M[j, i]) > max_val:
-                    max_row = j
-                    max_val = abs(M[j, i])
-          # Intercambiar la fila i con la fila del pivote
-          M[[i, max_row]] = M[[max_row, i]]
-          
-          # Hacer ceros debajo del pivote usando operaciones elementales de fila
-          
-          for j in range(i + 1, n):
-               factor = M[j, i] / M[i, i]
-               M[j] = M[j] - factor * M[i]
-               
-               
-          # Resolver el sistema usando sustitución hacia atrás
-     for i in range(n - 1, -1, -1):
-          
-          x[i] = M[i, 3] / M[i, i]
-          print(x)
-          for j in range(i - 1, -1, -1):
-               M[j, -1] -= M[j, i] * x[i]
-               
-     return x
-
-def eliminacion_gaussiana(A,B):
-     
-     # PROCEDIMIENTO
-     casicero = 1e-15 # Considerar como 0
-
-     # Evitar truncamiento en operaciones
-     A = np.array(A,dtype=float) 
-
-     # Matriz aumentada
-     AB  = np.concatenate((A,B),axis=1)
-     AB0 = np.copy(AB)
-
-     # Pivoteo parcial por filas
+     M  = np.concatenate((A,B),axis=1)
      tamano = np.shape(AB)
      n = tamano[0]
      m = tamano[1]
-
-     # Para cada fila en AB
+     
      for i in range(0,n-1,1):
-          # columna desde diagonal i en adelante
-          columna  = abs(AB[i:,i])
-          dondemax = np.argmax(columna)
           
-          # dondemax no está en diagonal
-          if (dondemax !=0):
-               # intercambia filas
-               temporal = np.copy(AB[i,:])
-               AB[i,:] = AB[dondemax+i,:]
-               AB[dondemax+i,:] = temporal
-     AB1 = np.copy(AB)
-
-     # eliminación hacia adelante
+          columna  = abs(M[i:,i])
+          max = np.argmax(columna)
+          
+          if (max !=0):
+               
+               temporal = np.copy(M[i,:])
+               M[i,:] = M[max+i,:]
+               M[max+i,:] = temporal
+               
      for i in range(0,n-1,1):
-          pivote   = AB[i,i]
+          
+          pivote   = M[i,i]
           adelante = i + 1
           for k in range(adelante,n,1):
-               factor  = AB[k,i]/pivote
-               AB[k,:] = AB[k,:] - AB[i,:]*factor
+               factor  = M[k,i]/pivote
+               M[k,:] = M[k,:] - M[i,:]*factor
                
-     # sustitución hacia atrás
      ultfila = n-1
      ultcolumna = m-1
-     X = np.zeros(n,dtype=float)
+     x = np.zeros(n,dtype=float)
      
      for i in range(ultfila,0-1,-1):
           suma = 0
           for j in range(i+1,ultcolumna,1):
-               suma = suma + AB[i,j]*X[j]
-          b = AB[i,ultcolumna]
-          X[i] = (b-suma)/AB[i,i]
+               suma = suma + M[i,j]*x[j]
+          b = M[i,ultcolumna]
+          x[i] = (b-suma)/M[i,i]
           
-     X = np.transpose([X])
-     # SALIDA
-     print('Matriz aumentada:')
-     print(AB0)
-     print('Pivoteo parcial por filas')
-     print(AB1)
-     print('eliminación hacia adelante')
-     print(AB)
-     print('solución: ')
-     print(X)
+     x = np.transpose([x])
+     
+     print('solución: ',x)
 
 
 
 
-A = np.array([[3,1,-1],
+A1 = np.array([[3,1,-1],
                [1,-2,1],
                [4,-1,1]])
 
-B = np.array([[2],
+A2 = np.array([[2],
                [0],
                [3]])
 
@@ -116,12 +59,6 @@ B1 = np.array([[1, 1, 1],
 B2 = np.array([[0],
                [0],
                [6]])
-
-#Ans = gauss(A1,A2)
-
-# Mostrar la solución
-
-
 
 x = eliminacion_gaussiana(A, B)
 print("Solución x:", x)
